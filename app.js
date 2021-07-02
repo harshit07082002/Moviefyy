7d7964afbda02878ec4eb3bb082017d0d18696fe
Harshit Arora <aroraharshit2002@gmail.com>
2021-07-02 22:38:13 +0530

    s

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv').config({ path: 'config.env' });
 app.use(express.static('public'));
 
 app.route('/').get((req, res) => {
   res.sendFile(path.join(__dirname, '/public/main.html'));
 });
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('listening');
 });
